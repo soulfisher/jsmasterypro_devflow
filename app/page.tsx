086d69a31404e5542b9ec0d5fe8eca61a78bdc05
Cassandra Cook <cassandracook@gmail.com>
2025-03-01 01:43:06 -0800

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of Next.js</h1>
+    <h1 className="text-3xl text-violet-700 font-black">
+      Welcome to the world of Next.js
+    </h1>
   );
 }
